b599406eaee7214b48e226c3690e43a8e94e859c
Amit Dhillon <amitdhillon5606@gmail.com>
2025-09-20 01:48:35 +0530

    Add cors origin

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 await connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is running'))
